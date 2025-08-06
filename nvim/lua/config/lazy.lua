b92f442f9616438c3450c8a35028fff6104be4ca
assdcii <alexander.grahamlel@proton.me>
2025-08-06 17:14:16 +1000

    i just want java to work

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "nvim-java/nvim-java" },
     -- import/override with your plugins
     { import = "plugins" },
   },
